e4a524d40824614dfc7db03511600d97258c302a
Mohamed Samb <mohameds@mit.edu>
2023-01-17 20:16:58 -0800

    created the HomePage

---
+++ client/src/components/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../utilities.css";
+import "./HomePage.css"
+
+const HomePage = () => {
+    return (
+        <div className="HomePage-container">
+            <div className="HomePage-leftside">
+                <div className="HomePage-circle">
+                    <img src="../../logo.png" />
+                </div>
+            </div>
+            <div className="HomePage-rightside">
+                <div className="HomePage-motto">
+                    <h2>STAY MOTIVATED</h2>
+                    <h2>STAY ACTIVE</h2>
+                    <h2>TAKE CONTROL</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -3,6 +3,7 @@ import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/goo
 
 import PreLoginNavbar from "../modules/PreLoginNavbar";
 import PostLoginNavbar from "../modules/PostLoginNavbar";
+import HomePage from "./HomePage";
 
 import "../../utilities.css";
 import "./Skeleton.css";
@@ -11,27 +12,11 @@ import "./Skeleton.css";
 const GOOGLE_CLIENT_ID = "FILL ME IN";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
+  let currentPage = (<HomePage />);
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       {userId ? (<PostLoginNavbar handleLogout={handleLogout}/>) : (<PreLoginNavbar handleLogin={handleLogin}/>)}
-      <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.us/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app</h2>
-      <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
-        Check out this getting started guide
-      </a>
+      {currentPage}
     </GoogleOAuthProvider>
   );
 };
