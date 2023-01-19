cd3021766091f09c36d2663e3784d0b9e0c5b836
Mohamed Samb <mohameds@mit.edu>
2023-01-18 17:01:04 -0800

    Created an about page; Made prelogin navbar functional

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import PreLoginNavbar from "../modules/PreLoginNavbar";
 import PostLoginNavbar from "../modules/PostLoginNavbar";
 import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
 
 import "../../utilities.css";
 import "./Skeleton.css";
@@ -12,10 +13,16 @@ import "./Skeleton.css";
 const GOOGLE_CLIENT_ID = "FILL ME IN";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
-  let currentPage = (<HomePage />);
+  const [page, setPage] = useState("Home");
+  let currentPage = undefined;
+  if(page === "Home"){
+    currentPage = (<HomePage />)
+  } else if(page === "About"){
+    currentPage = (<AboutPage />)
+  }
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      {userId ? (<PostLoginNavbar handleLogout={handleLogout}/>) : (<PreLoginNavbar handleLogin={handleLogin}/>)}
+      {userId ? (<PostLoginNavbar handleLogout={handleLogout} />) : (<PreLoginNavbar handleLogin={handleLogin} setPage={setPage} />)}
       {currentPage}
     </GoogleOAuthProvider>
   );
